99769a22c8fea8af8f9e366e8062e0d8c6e69cb9
Gardtheod <cvilcapazaz@ulasalle.edu.pe>
2020-04-08 16:44:18 -0500

    Ejercicio 4

---
+++ Laboratorios/PL02-04/PL02-04.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+void p(int cant){
+float price=10;
+float total=cant*price;
+float compra=cant*price, desc;
+int candy;
+if(cant < 8){
+    desc=total*6.5/100;
+    total=total-desc;
+}else if(cant>=8 && cant<15){
+    desc=total*9/100;
+    total=total-desc;
+}else if(cant>=15){
+    desc=total*11.5/100;
+    total=total-desc;
+}
+if (total>380){
+    candy=3*cant;
+}else{
+    candy=2*cant;
+}
+cout<<"Importe de compra es: "<<compra<<", Descuento es de: "<<desc<<", Importe a pagar es:"<<total<<", Cantidad de caramelos de regalo: "<<candy<<endl;
+}
+void d(int cant){
+float price=7;
+float total=cant*price;
+float compra=cant*price, desc;
+int candy;
+if(cant < 8){
+    desc=total*6.5/100;
+    total=total-desc;
+}else if(cant>=8 && cant<15){
+    desc=total*9/100;
+    total=total-desc;
+}else if(cant>=15){
+    desc=total*11.5/100;
+    total=total-desc;
+}
+if (total>380){
+    candy=3*cant;
+}else{
+    candy=2*cant;
+}
+cout<<"Importe de compra es: "<<compra<<", Descuento es de: "<<desc<<", Importe a pagar es:"<<total<<", Cantidad de caramelos de regalo: "<<candy<<endl;
+}
+void t(int cant){
+float price=12.5;
+float total=cant*price;
+float compra=cant*price, desc;
+int candy;
+if(cant < 8){
+    desc=total*6.5/100;
+    total=total-desc;
+}else if(cant>=8 && cant<15){
+    desc=total*9/100;
+    total=total-desc;
+}else if(cant>=15){
+    desc=total*11.5/100;
+    total=total-desc;
+}
+if (total>380){
+    candy=3*cant;
+}else{
+    candy=2*cant;
+}
+cout<<"Importe de compra es: "<<compra<<", Descuento es de: "<<desc<<", Importe a pagar es:"<<total<<", Cantidad de caramelos de regalo: "<<candy<<endl;
+}
+
+
+int main(){
+int cant;
+char ch;
+cin>>cant;
+cin>>ch;
+if (ch=='p'){
+    p(cant);
+}else if(ch=='d'){
+    d(cant);
+}else if(ch=='t'){
+    t(cant);
+}
+
+return 0;
+}
